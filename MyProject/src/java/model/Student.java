aae03c8248932896957ee2156c3385b3cfe3136f
segay2vn <tonnahe171051@fpt.edu.vn>
2023-10-06 00:11:55 +0700

    Class Student has defined

---
+++ MyProject/src/java/model/Student.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author pc
+ */
+public class Student {
+    private String name;
+    private String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "name=" + name + ", id=" + id + '}';
+    }
+    
+}
