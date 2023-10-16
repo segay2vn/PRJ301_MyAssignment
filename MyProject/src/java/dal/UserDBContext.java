281c0c4af34bbe7d5a3e0aa028d20b0cdfab91ca
segay2vn <tonnahe171051@fpt.edu.vn>
2023-10-16 11:21:34 +0700

    Database connect for login

---
+++ MyProject/src/java/dal/UserDBContext.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.User;
+
+/**
+ *
+ * @author pc
+ */
+public class UserDBContext extends DBContext<User> {
+
+    @Override
+    public void insert(User model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(User model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void remove(User model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public User get(User model) {
+        try {
+            String sql = "SELECT username,displayname FROM [User]\n"
+                    + "WHERE username = ? AND [password] = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setString(1, model.getUsername());
+            stm.setString(2, model.getPassword());
+            ResultSet rs = stm.executeQuery();
+            if (rs.next()) {
+                User user = new User();
+                user.setUsername(model.getUsername());
+                user.setDisplayname(rs.getString("displayname"));
+                return user;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public ArrayList<User> list() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
