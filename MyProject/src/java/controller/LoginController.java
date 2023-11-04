5567e7c5aade73b09c1df1cfdaad5b46424beb66
segay2vn <tonnahe171051@fpt.edu.vn>
2023-11-05 00:54:51 +0700

    LoginController next fixed

--- MyProject/src/java/controller/LoginController.java
+++ MyProject/src/java/controller/LoginController.java
@@ -8,23 +8,42 @@ import dal.UserDBContext;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import model.User;
 
 /**
  *
- * @author pc
+ * @author sonnt
  */
 public class LoginController extends HttpServlet {
 
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         request.getRequestDispatcher("view/login.jsp").forward(request, response);
     }
 
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -35,11 +54,39 @@ public class LoginController extends HttpServlet {
         param.setPassword(password);
         UserDBContext db = new UserDBContext();
         User loggedUser = db.get(param);
+        int loggedUser1 = db.getId(param);
+        String loggedUser2 = db.getName(param);
         if (loggedUser != null) {
-            request.getRequestDispatcher("view/home.jsp").forward(request, response);
+            
+            HttpSession session = request.getSession();
+            session.setAttribute("account", loggedUser);
+            session.setAttribute("uid", loggedUser1);
+            session.setAttribute("username", loggedUser2);
+            
+            String remember = request.getParameter("remember");
+            if (remember != null) {
+                Cookie c_user = new Cookie("user", username);
+                Cookie c_pass = new Cookie("pass", password);
+                c_user.setMaxAge(24 * 3600);
+                c_pass.setMaxAge(24 * 3600);
+                response.addCookie(c_user);
+                response.addCookie(c_pass);
+            }
+            response.sendRedirect("view/home.jsp");
         } else {
-            request.getRequestDispatcher("view/login.jsp").forward(request, response);
+            response.sendRedirect("view/InvalidLogin.jsp");
         }
+
     }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
 
 }
